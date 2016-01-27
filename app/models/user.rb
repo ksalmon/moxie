d3a7faee1ac8ca68035ad58770829f2340bf332d
ksalmon <kyleraysalmon@gmail.com>
2016-01-26 21:12:43 -0500

    Add Validations and Update Form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :user_name, presence: true, :uniqueness => { :case_sensitive => false }
+  validates :email, presence: true, format: { with: Devise.email_regexp }
+
+  before_save :assign_api_key
+
+  def assign_api_key
+    if api_key.blank?
+      self.api_key = generate_api_key
+    end
+  end
+
+  def generate_api_key
+    loop do
+      api_key = Devise.friendly_token
+      break api_key unless User.where(api_key: api_key).first
+    end
+  end
 end
