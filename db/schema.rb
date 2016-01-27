cabd7cb6333cec51856a847d01d689caad4454ce
ksalmon <kyleraysalmon@gmail.com>
2016-01-26 22:08:51 -0500

    Add company_id to store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127024043) do
+ActiveRecord::Schema.define(version: 20160127030750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160127024043) do
     t.string   "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "company_id"
   end
 
 end
